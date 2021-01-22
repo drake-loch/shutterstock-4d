f837f1958df9b6f02a64a5c05053c3037ac3529c
drake <tehtrojenguy@gmail.com>
2021-01-22 11:36:05 -0600

    added feature

---
+++ src/Components/Overlay/Overlay.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Overlay.scss';
+
+export class Overlay extends Component {
+    constructor(){
+        super();
+
+    }
+
+    openMenu = () =>{
+        console.log("Menu has opened");
+    }
+    render() {
+        return (
+            <div className="overlay">
+                <div onClick={this.openMenu} className="overlay-bttn"></div>
+            </div>
+        )
+    }
+}
+
+export default Overlay
